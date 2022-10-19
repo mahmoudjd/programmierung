29b0db4e5edcc7db4e8f5f14c16137fc095ec392
mahmoudjd <mahmoudjd452@gmail.com>
2022-10-19 13:51:58 +0200

    pyramid numbers  add

--- pyramid.c
+++ pyramid.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+void numbers(int n) { 
+  for (int i = 1; i <= n; i++) { 
+    for (int j = n; j >= 1; j--) {
+      if (i >= j) {
+        printf("%d ", i);
+      } else { 
+        printf(" ");
+      }
+    }
+    printf("\n");
+  }
+}
+
 int main() { 
   int n = 15;
   for(int i = 0; i < n; i++) { 
@@ -12,5 +25,9 @@ int main() {
     }
     printf("\n");
   }
+
+  printf("\n");
+
+  numbers(9);
   return 0;
 }
